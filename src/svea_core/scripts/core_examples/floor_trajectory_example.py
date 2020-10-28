54c2525b6ac2951fb1b878eea9363063706fda14
Bianca Otake <bianca.otake@einride.tech>
2020-10-28 16:51:38 +0100

    team5 task credentials -bianca

---
+++ src/svea_core/scripts/core_examples/floor_trajectory_example.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+from svea.svea_managers.path_following_sveas import SVEAPurePursuit
+from svea.states import VehicleState
+from svea.localizers import LocalizationInterface
+from svea.controllers.pure_pursuit import PurePursuitController
+from svea.data import BasicDataHandler, TrajDataHandler, RVIZPathHandler
+from svea.models.bicycle import SimpleBicycleModel
+from svea.simulators.sim_SVEA import SimSVEA
+
+__team__ = "Team 5"
+__maintainers__ ="Bianca Otake, ...”
+__status__ ="Development"
+
+
+## SIMULATION PARAMS ##########################################################
+vehicle_name = "SVEA"
+target_velocity = 1.0 # [m/s]
+dt = 0.01 # frequency of the model updates
+
+#TODO: create a trajectory that goes around the track
+xs = [-7.4 , -2.33, 10.3, 5.9, -7.2, -2.5]
+ys = [-15.3,  -7.09, 11.4, 14.8, -4.2, -7.6]
+traj_x = []
+traj_y = []
+for i in range(0,len(xs)-1):
+    traj_x += np.linspace(xs[i], xs[i+1]).tolist()
+    traj_y += np.linspace(ys[i], ys[i+1]).tolist()
+
+###############################################################################
+
+## INIT #######################################################################
+default_init_pt = [0.0, 0.0, 0.0, 0.0] # [x, y, yaw, v], units: [m, m, rad, m/s]
+###############################################################################
+
+
+def param_init():
+    """Initialization handles use with just python or in a launch file
+    """
+    # grab parameters from launch-file
+    start_pt_param = rospy.search_param('start_pt')
+    is_sim_param = rospy.search_param('is_sim')
+    use_rviz_param = rospy.search_param('use_rviz')
+    use_matplotlib_param = rospy.search_param('use_matplotlib')
+
+    start_pt = rospy.get_param(start_pt_param, default_init_pt)
+    if isinstance(start_pt, str):
+        start_pt = start_pt.split(',')
+        start_pt = [float(curr) for curr in start_pt]
+        start_pt = VehicleState(*start_pt)
+
+    is_sim = rospy.get_param(is_sim_param, True)
+    use_rviz = rospy.get_param(use_rviz_param, False)
+    use_matplotlib = rospy.get_param(use_matplotlib_param, False)
+
+    return start_pt, is_sim, use_rviz, use_matplotlib
+
+
+def main():
+    rospy.init_node('floor2_example')
+    start_pt, is_sim, use_rviz, use_matplotlib = param_init()
+
+    # select data handler based on the ros params
+    if use_rviz:
+        DataHandler = RVIZPathHandler
+    elif use_matplotlib:
+        DataHandler = TrajDataHandler
+    else:
+        DataHandler = BasicDataHandler
+
+    if is_sim:
+        # start the simulation
+        model_for_sim = SimpleBicycleModel(start_pt)
+        simulator = SimSVEA(vehicle_name, model_for_sim,
+                            dt=dt, start_paused=True).start()
+
+    # start pure pursuit SVEA manager
+    svea = SVEAPurePursuit(vehicle_name,
+                           LocalizationInterface,
+                           PurePursuitController,
+                           traj_x, traj_y,
+                           data_handler = DataHandler)
+    svea.start(wait=True)
+
+    if is_sim:
+        # start simulation
+        simulator.toggle_pause_simulation()
+
+    # simualtion loop
+    svea.controller.target_velocity = target_velocity
+    while not svea.is_finished and not rospy.is_shutdown():
+        state = svea.wait_for_state()
+
+        # compute control input via pure pursuit
+        steering, velocity = svea.compute_control()
+        svea.send_control(steering, velocity)
+
+        # visualize data
+        if use_matplotlib or use_rviz:
+            svea.visualize_data()
+        else:
+            rospy.loginfo_throttle(1, state)
+
+    if not rospy.is_shutdown():
+        rospy.loginfo("Trajectory finished!")
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
