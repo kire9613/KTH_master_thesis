a36967faa9de99bf6c5c0f07066a5c8b1b9df770
Roberto Castro Sundin <rosun@kth.se>
2020-11-03 08:56:51 +0000

    Add run_lidar param. Set to true by default

--- src/svea_core/scripts/core_examples/floor2_team1.py
+++ src/svea_core/scripts/core_examples/floor2_team1.py
@@ -55,6 +55,7 @@ def param_init():
     is_sim_param = rospy.search_param('is_sim')
     use_rviz_param = rospy.search_param('use_rviz')
     use_matplotlib_param = rospy.search_param('use_matplotlib')
+    run_lidar_param = rospy.search_param('run_lidar')
 
     start_pt = rospy.get_param(start_pt_param, default_init_pt)
     if isinstance(start_pt, str):
@@ -65,13 +66,14 @@ def param_init():
     is_sim = rospy.get_param(is_sim_param, True)
     use_rviz = rospy.get_param(use_rviz_param, False)
     use_matplotlib = rospy.get_param(use_matplotlib_param, False)
+    run_lidar = rospy.get_param(run_lidar_param, True)
 
-    return start_pt, is_sim, use_rviz, use_matplotlib
+    return start_pt, is_sim, use_rviz, use_matplotlib, run_lidar
 
 
 def main():
     rospy.init_node('floor2_example')
-    start_pt, is_sim, use_rviz, use_matplotlib = param_init()
+    start_pt, is_sim, use_rviz, use_matplotlib, _run_lidar = param_init()
 
     # select data handler based on the ros params
     if use_rviz:
@@ -85,7 +87,7 @@ def main():
         # start the simulation
         model_for_sim = SimpleBicycleModel(start_pt)
         simulator = SimSVEA(vehicle_name, model_for_sim,
-                            dt=dt, start_paused=True).start()
+                            dt=dt, start_paused=True, run_lidar=_run_lidar).start()
 
     # start pure pursuit SVEA manager
     svea = SVEAPurePursuit(vehicle_name,
