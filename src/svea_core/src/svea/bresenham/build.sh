e80c5ef670da035fdab7cbd32f9c2dff1102b295
Roberto Castro Sundin <rosun@kth.se>
2020-12-21 17:26:50 +0100

    Binaries for x86_64 architecture

--- src/svea_core/src/svea/bresenham/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ev
-rm bresenham.so
-rm __init__.py
-rm __init__.pyc
-python2 setup.py build_ext --inplace
-touch __init__.py
--- src/svea_core/src/svea/bresenham/setup.py
+++
@@ -1,6 +0,0 @@
-import distutils.core
-import Cython.Build
-distutils.core.setup(
-    ext_modules = Cython.Build.cythonize("bresenham.pyx",
-                                         build_dir="build"
-                                         ))
